38ecf046430cee30ca4c6ed21f9eb9eb50f99922
Aarsh-s-Lal <158296285+Aarsh-s-Lal@users.noreply.github.com>
2024-04-17 04:59:04 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,9 +4,12 @@ $username = 'admin';
 $password = 'aarsh123';
 $database = 'mydb';
 
-$conn = new mysqli($host, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
